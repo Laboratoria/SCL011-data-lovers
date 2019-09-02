10ea6fa1d6f232864e942b2efc38d3303e1f5981
crusiris <cruzsirys@gmail.com>
2019-09-01 20:20:04 -0400

    name + img desde javascript

--- src/data.js
+++ src/data.js
@@ -4,7 +4,30 @@
 // puedes ver como agregamos la función a nuestro objeto global window
 
 const example = () => {
-  return 'example';
+    console.log(RICKANDMORTY.results[0].name);
+    botonEnvio = document.querySelector('[type="button"]');
+
+    // nuevoItem = document.querySelector('[type="text"]');
+    nuevoItem = RICKANDMORTY.results[0].name;
+    listaCompra = document.getElementById("listaCompra");
+
+    botonEnvio.addEventListener("click", anadir);
 };
 
-window.example = example;
+function anadir(e) {
+    evento = e || window.event;
+    if (nuevoItem.value == "") {
+        evento.preventDefault();
+    } else {
+        var lista = document.createElement("li");
+        var x = document.createElement("IMG");
+        lista.innerHTML = nuevoItem;
+        // lista.addEventListener("dblclick",eliminarLi);
+        listaCompra.appendChild(lista);
+        x.setAttribute("src", RICKANDMORTY.results[0].image);
+        document.body.appendChild(x);
+        nuevoItem.value = "";
+    }
+}
+
+window.onload = example;
