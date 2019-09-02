b98beea8415ed2e93f1d3450cf9424b6e9f641a1
conyzarate <cony.zarate@gmail.com>
2019-09-02 15:22:23 -0400

    cambio de carpeta js

--- src/data.js
+++ src/data.js
@@ -3,49 +3,48 @@
 // esta es una función de ejemplo
 // puedes ver como agregamos la función a nuestro objeto global window
 
-const data = () => {
-    //imprimo mi Json en consola para verla y guiarme
-    // console.log(RICKANDMORTY.results);
+// const data = () => {
+    
 
-    //Creo una variable boton y con el querySelector accedo a el valor  
-    button = document.querySelector('[type="button"]');
-    console.log(button);
-    //Asigno el valor del nombre 
-    nuevoItem = RICKANDMORTY.results[0].name;
-    //Asino el valor de la imagen
-    photo = RICKANDMORTY.results[0].image;
-    // Defino el bloque donde voy a insertar la data
-    dataElement = document.getElementById("element");
+//     //Creo una variable boton y con el querySelector accedo a el valor  
+//     button = document.querySelector('[type="button"]');
+//     nuevoItem = RICKANDMORTY.results[0].name;
+//     photo = RICKANDMORTY.results[0].image;
+//     genero = RICKANDMORTY.results[0].gender; 
+//     dataElement = document.getElementById("element");
 
-    //Le asigno el evento y la funcion al boton 
-    button.addEventListener("click", anadir);
-};
+//     //Le asigno el evento y la funcion al boton 
+//     button.addEventListener("click", anadir);
+// };
 
-//esta funcion recibe un evento 
-function anadir(e) {
-    // guardo el evento
-    evento = e || window.event;
-    //si es vacio no me hace nada (borre el imput)
-    if (nuevoItem.value == "") {
-        evento.preventDefault();
-    } else {
-        //Creo los elementos 
-        let name = document.createElement("p");
-        let photo = document.createElement("IMG");
+// function anadir(e) {
+//     // guardo el evento
+//     evento = e || window.event;
+//     //si es vacio no me hace nada (borre el imput)
+//     if (nuevoItem.value == "") {
+//         evento.preventDefault();
+//     } else {
+//         //Creo los elementos 
+//         let name = document.createElement("p");
+//         let photo = document.createElement("IMG");
+//         let gender = document.createElement("p");
+        
 
-        //Asigno el contenido de name
-        name.innerHTML = nuevoItem;
+//         //Asigno el contenido de name
+//         name.innerHTML = nuevoItem;
 
-        //Asigno el contenido de photo
+//         //Asigno el contenido de photo
 
 
-        //Agrego al html
-        dataElement.appendChild(name);
-        photo.setAttribute("src", RICKANDMORTY.results[0].image);
-        dataElement.appendChild(photo);
-        nuevoItem.value = "";
-     }
- }
+//         //Agrego al html
+//         dataElement.appendChild(name);
+//         photo.setAttribute("src", RICKANDMORTY.results[0].image);
+//         dataElement.appendChild(photo);
+//       dataElement.appendChild(gender);
 
-window.onload = data;
+//         nuevoItem.value = "";
+//      }
+//  }
+
+// window.onload = data;
 
--- src/main.js
+++ src/main.js
@@ -1,2 +1,46 @@
-/* Manejo del DOM */
+const ricks = RICKANDMORTY;
 
+const data = () => {
+    
+
+    //Creo una variable boton y con el querySelector accedo a el valor  
+    button = document.querySelector('[type="button"]');
+    nuevoItem = RICKANDMORTY.results[0].name;
+    photo = RICKANDMORTY.results[0].image;
+    genero = RICKANDMORTY.results[0].gender; 
+    dataElement = document.getElementById("element");
+
+    //Le asigno el evento y la funcion al boton 
+    button.addEventListener("click", anadir);
+};
+
+function anadir(e) {
+    // guardo el evento
+    evento = e || window.event;
+    //si es vacio no me hace nada (borre el imput)
+    if (nuevoItem.value == "") {
+        evento.preventDefault();
+    } else {
+        //Creo los elementos 
+        let name = document.createElement("p");
+        let photo = document.createElement("IMG");
+        let gender = document.createElement("p");
+        
+
+        //Asigno el contenido de name
+        name.innerHTML = nuevoItem;
+
+        //Asigno el contenido de photo
+
+
+        //Agrego al html
+        dataElement.appendChild(name);
+        photo.setAttribute("src", RICKANDMORTY.results[0].image);
+        dataElement.appendChild(photo);
+      dataElement.appendChild(gender);
+
+        nuevoItem.value = "";
+     }
+ }
+
+window.onload = data;
