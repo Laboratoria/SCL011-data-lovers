c96aef0ba732c9a5249ee1fa03d932427706eec9
macarenasaez <makadaira@gmail.com>
2019-09-05 16:08:29 -0400

    imprimiendo champion

--- src/data.js
+++ src/data.js
@@ -3,17 +3,9 @@
 // esta es una función de ejemplo
 // puedes ver como agregamos la función a nuestro objeto global window
 
-const dataArray= (dataLOL) => {
+const nombrex = () => {
 
-  return Object.values(dataLOL["data"]) ;
+  return ;
 };
 
-const mostPawer =(data,getAttack)=>{
-  let pawued=[];
-  for(let i = 0; i < data.length; i ++){
-    if(data[i]["info"][getAttack]==10){
-      pawued.push(data[i]);
-    }
-  }
-}
 window.LOL = LOL;
--- src/main.js
+++ src/main.js
@@ -1,36 +1,15 @@
-
-const lolData = Object.values(LOL.data);
-
 /* Manejo del DOM */
+const lolData = Object.values(LOL.data);
+window.lolData = lolData
 
-const lolData = window.LEAGUEOFLEGENDS.result;
+const btnchampion = document.getElementById("btnchampion");
+//funcion click
+btnchampion.addEventListener("click",() =>{
+    for (let i = 0; i < lolData.length; i++) {
+    root.innerHTML += `
 
-const container = document.getElementById("root");
-document.getElementById(leagueoflegends).addEventListener("click",() =>{
+        <img src="${lolData[i].img}">`
+        
+    }
+    })
 
-const result = legendsFilter(lolData,legendsFilter);
-container.innerHTML = "";
-
-result.forEach(element => {
-    container.innerHTML += 
-    
-    `<div class="flip-card">
-        <div class="flip-card-inner">
-
-        <div class="flip-card-front">
-        < div img id="img-card" src={element.image} alt="Imagen">     
-   <h4>${element.name}</h4> 
-   <p>${element.attack}</p>
-   <p>${element.defense}</p>
-   <p>${element.magic}</p>
-   <p>${Element.difficulty}</p>
-   <p>${element.tags}</p>
-   <p>${element.partype}</p>
-    
-   </div>
-   </div>
-    </div>
-    </div>`
-});
-
-});
