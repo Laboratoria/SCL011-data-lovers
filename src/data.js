5ae06f7f7f083206e0506c2939b99c0f83bf1667
María Beltrán <mariabeltra.98@gmail.com>
2019-09-06 09:10:40 -0400

    funcion filtrado y mostrar noticias

--- src/data.js
+++ src/data.js
@@ -1,10 +1,61 @@
-/* Manejo de data */
-
-// esta es una función de ejemplo
-// puedes ver como agregamos la función a nuestro objeto global window
-
-const example = () => {
-  return 'example';
+window.filterData={
+  filter: (takeData)=>{
+      
+      let concatenartf2="";
+      let concatenarUpdate="";
+      let concatenarPcgamer="";
+      let concatenarEurogamer="";
+      let concatenarRps = "";
+     // arrow =[concatenartf2,concatenarUpdate,concatenarPcgamer,concatenarEurogamer,concatenarRps];
+     
+     for (let i=0; i<takeData.length;i++){
+      let changesData = document.createElement("div")
+      changesData.className="create-cards"
+      let newsCards = document.createElement("div");
+      newsCards.className =  "cards";
+      let titleData= document.createElement("h4")
+      titleData.textContent= takeData[i].title
+      //console.log (titleData)
+      let newsContent= document.createElement("p")
+      newsContent.textContent= takeData[i].contents
+      //console.log(newsContent)
+      // armemos
+      //const contenedor = document.getElementById("conte");
+      //contenedor.innerHTML= `<p> Hola </p>`
+      changesData.appendChild(newsCards);
+      newsCards.appendChild(titleData)
+      newsCards.appendChild(newsContent)
+      console.log(newsCards.appendChild(newsContent))
+      const design = document.getElementById("content").appendChild(changesData).innerHTML;
+         //const arrow =[];
+              if (takeData[i].feedlabel =="TF2 Blog"){
+              concatenartf2+= design;
+             // finalResult = concatenartf2;
+          }
+          if (takeData[i].feedlabel=="Product Update"){
+              concatenarUpdate+= design;
+              //inalResult = concatenarUpdate;
+              //console.log(concatenarUpdate)
+          }
+        if (takeData[i].feedlabel==="PC Gamer"){
+            concatenarPcgamer+= design;
+            //finalResult = concatenarPcgamer;
+           // console.log(concatenarPcgamer)
+        }
+        if (takeData[i].feedlabel==="Eurogamer"){
+          concatenarEurogamer+= design;
+          //finalResult=concatenarEurogamer;
+          //console.log(concatenarEurogamer)
+        }
+        if (takeData[i].feedlabel==="Rock, Paper, Shotgun"){
+            concatenarRps+= design;
+           // finalResult=concatenarRps;
+         // console.log(concatenarRps); 
+        }
+       
+      }return {concatenartf2,concatenarUpdate,concatenarPcgamer,concatenarEurogamer,concatenarRps}
+      //console.log(concatenartf2)
+  }
 };
 
-window.example = example;
+// pruebas de dar forma a la data
--- src/main.js
+++ src/main.js
@@ -1,1 +1,24 @@
 /* Manejo del DOM */
+const takeData = STEAM.appnews.newsitems;
+
+//btn
+const blogtf2btn = document.getElementById("blogtf2")
+blogtf2btn.addEventListener ('click',()=>{
+    document.getElementById("content").innerHTML = window.filterData.filter(takeData).concatenartf2;
+})
+const steambtn = document.getElementById("steam")
+steambtn.addEventListener ('click',()=>{
+    document.getElementById("content").innerHTML = window.filterData.filter(takeData).concatenarUpdate;
+})
+const eurobtn = document.getElementById("euro")
+eurobtn.addEventListener ('click',()=>{
+    document.getElementById("content").innerHTML = window.filterData.filter(takeData).concatenarEurogamer;
+})
+const pcbtn = document.getElementById("PC")
+pcbtn.addEventListener ('click',()=>{
+    document.getElementById("content").innerHTML = window.filterData.filter(takeData).concatenarPcgamer;
+})
+const rpsbtn = document.getElementById("rps")
+rpsbtn.addEventListener ('click',()=>{
+    document.getElementById("content").innerHTML = window.filterData.filter(takeData).concatenarRps;
+})
