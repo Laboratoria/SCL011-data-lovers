25119b45b78e9425eef8f29287681663119fff13
marciaalegria <ytasegundo@gmail.com>
2019-09-10 08:31:25 -0300

    funcion botones

--- src/main.js
+++ src/main.js
@@ -1,17 +1,38 @@
-
-// document.getElementById("lolData").innerHTML= "";
-/* Manejo del DOM */
-const lolData = Object.values(LOL.data);
-window.lolData = lolData
-
+//boton champion
 const btnchampion = document.getElementById("btnchampion");
 //funcion click
 btnchampion.addEventListener("click",() =>{
     for (let i = 0; i < lolData.length; i++) {
-    root.innerHTML += `
-
-        <img src="${lolData[i].img}">`
-        
+        root.innerHTML += `
+       <div>
+            <div id="img${[i]}">
+                <img src="${lolData[i].img}">
+            </div>
+            <div id="name${[i]}">
+                <p>${lolData[i].id}</p>
+            </div> 
+        </div>`;
+       //console.log(lolData[i].id);
+       
     }
-    })
+});
 
+const btnMostPower = document.getElementById("btnMostPower");
+btnMostPower.addEventListener("click",() =>{
+    mostPower(lolData, "attack");
+});
+
+const btnFighter = document.getElementById("btnFighter");
+btnFighter.addEventListener("click",() =>{
+    tags(lolData, "Fighter");
+});
+
+const btnStats =document.getElementById("stats");
+btnStats.addEventListener("click",()=>{
+    stats(lolData, "stats");
+});
+
+const sortDataLol = document.getElementById("a-z");
+sortDataLol.addEventListener("onchange",()=>{
+    sortData(lolData, "z-a");
+});
