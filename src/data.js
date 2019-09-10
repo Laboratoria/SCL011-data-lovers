d06d557b875b54f589573b13637d6d754a5b629f
macarenasaez <makadaira@gmail.com>
2019-09-10 09:31:55 -0300

    Agrego css a tarjetas

--- src/data.js
+++ src/data.js
@@ -1,11 +1,6 @@
-/* Manejo de data */
 
-// esta es una función de ejemplo
-// puedes ver como agregamos la función a nuestro objeto global window
 
-const nombrex = () => {
 
-  return ;
-};
 
-window.LOL = LOL;
+window.LOL = LOL 
+
--- src/main.js
+++ src/main.js
@@ -7,9 +7,43 @@ const btnchampion = document.getElementById("btnchampion");
 btnchampion.addEventListener("click",() =>{
     for (let i = 0; i < lolData.length; i++) {
     root.innerHTML += `
-
-        <img src="${lolData[i].img}">`
+<div>
+<div class="flip-card">
+<div class="flip-card-front">
+<div id="img${[i]}">
+        <img src="${lolData[i].img}">
+        </div>
+        <div id="name${[i]}">
+        <p>${lolData[i].id}</p>
+        </div>
+        </div>`;
         
     }
     })
 
+  
+    
+    
+
+
+  /* const selectlegends = document.getElementByI("lolselect")
+    //funcion change
+    selectlegends.addEventListener("change",() =>{
+        const condition = selectlegends.value 
+        let results = window.filterT(lolData, condition);
+        document.getElementById("container").innerHTML=''; 
+        
+        createCards(results);  
+
+
+
+    })
+
+      /*  for (let i = 0; i < lolData.length; i++) {
+            container.innerHTML += `
+
+            <div class="flip-card">*/
+  
+
+
+  
