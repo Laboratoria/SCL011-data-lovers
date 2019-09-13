3bb1f02145e50aba94e66cd5baa9611761c606db
Your Name <you@example.com>
2019-09-13 10:13:30 -0300

    agrego cambios html

--- src/main.js
+++ src/main.js
@@ -10,20 +10,14 @@
 
 // crear una costante para todo(que es all) que sea igual a POKEMON (por la constante de la data que nos entregaron)
 // es .pokemon porque dentro de ("pokemon") se alojan todos los demas datos. Es un arreglo con muchos elementos adentro.
-const allData = POKEMON.pokemon;
+const data = POKEMON.pokemon;
 //se usa console log para revisar el todo (all) en la consola.
-console.log(allData);
+//console.log(data);
 
-window.onload = mostrar;
-/* obtener el elemento por id*/
-function mostrar() {
-    document.getElementById("pantallaInicio").style.display = "block";
-    document.getElementById("menu-header").style.display = "none";
-}
-
-/* obtener el elemento por id*/
-document.getElementById("pokebola").addEventListener('click', (evento) => {
-    evento.preventDefault();
-    document.getElementById("pantallaInicio").style.display = "none";
-    document.getElementById("menu-header").style.display = "block";
-});
+//Mostrar toda la data en CARDS
+const showData = (data) => {
+    //Va ir guardado los datos entregados
+    let result = "";
+    console.log(data);
+};
+window.onload = showData(data);
