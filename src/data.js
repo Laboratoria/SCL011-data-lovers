e95091fb7122b2f78e8f6d60b8619f2c18c7494e
María Beltrán <mariabeltra.98@gmail.com>
2019-09-23 11:48:49 -0300

    responsive y comentarios en el código

--- src/data.js
+++ src/data.js
@@ -1,17 +1,20 @@
 window.filterData={
-  
+  // Función para filtrar según Pagina de Origen
   filter: (takeData, buttonFilters)=>{
       let result= []; 
       for (let i=0; i<takeData.length;i++){
+        // aquí le decimos que separare cada valor del atributo feedlabel
           if (buttonFilters === takeData[i].feedlabel){
               result.push(takeData[i])
           }
+          // aquí le decimos que muestre todas las noticias
           if(buttonFilters === 'feedlabel'){
             result.push(takeData[i])
           }
       }
     return result
   }, 
+  // Función para ordenar las noticias por su Fecha
   sortByDate: (array, selectSort)=>{
     return array.sort(function(a,b){
       if (selectSort ==="recent"){
@@ -23,6 +26,7 @@ window.filterData={
       }
     });
   },
+  // Función para la estadistica
   statistics: (filteredData, takeData, buttonFilters)=>{
     filteredData = window.filterData.filter(takeData, buttonFilters);
     let statisticsEquation= filteredData.length*100/takeData.length
