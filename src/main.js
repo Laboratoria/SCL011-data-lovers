7fd06e2a2882f89d4b328977218a393d9565db75
Your Name <you@example.com>
2019-09-23 08:28:39 -0300

    agrego main con funcion mostrar data

--- src/main.js
+++ src/main.js
@@ -1,11 +1,55 @@
 /* Manejo del DOM */
-<<<<<<< HEAD
-=======
+const data = POKEMON.pokemon;
+const containerRoot = document.getElementById("root");
+const selectType = document.getElementById("type");
+
+/* obtener el elemento por id*/
+function mostrar() {
+    document.getElementById("pantallaInicio").style.display = "block";
+    document.getElementById("pantallaBienvenida").style.display = "none";
+    document.getElementById("menu2").style.display = "none";
+}
+document.getElementById("next").addEventListener('click', (evento) => {
+    evento.preventDefault();
+    document.getElementById("pantallaInicio").style.display = "none";
+    document.getElementById("pantallaBienvenida").style.display = "block";
+    document.getElementById("menu2").style.display = "none";
+});
+document.getElementById("ir").addEventListener('click', (evento) => {
+    evento.preventDefault();
+    document.getElementById("pantallaInicio").style.display = "none";
+    document.getElementById("pantallaBienvenida").style.display = "none";
+    document.getElementById("menu2").style.display = "block";
+});
+
+//Mostrar toda la data en CARDS
+const showData = (data) => {
+    //Va ir guardado los datos entregados
+    let result = "";
+    //console.log(data);
+    data.forEach(element => {
+        //element -->DATA[i]
+        //console.log(element.name);
+        result = containerRoot.innerHTML += `
+        <div>
+        <div class="card">
+        <div class="card-header">
+            <div class="img">
+                <img src="${element.img}">
+            </div>
+            <div class="card-body"
+            <h3>${element.num}</h3>
+            <h2>${element.name}</h2>
+            <p>${element.type}</p>
+            </div>
+        </div>
+        </div>
+        </div> `
+
+    });
+    return result;
+}
 
 
-
-
-  
-
-
->>>>>>> 44091839d9905782f3f98e23e2cfe0fbc3fb4b82
+window.onload = mostrar;
+window.onload = showData(data);
